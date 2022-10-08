d0940bba47c227b9c1b01c962887bf1a0024521a
Infarh <shmachilin@yandex.ru>
2022-10-08 21:05:13 +0300

    Доработка визуальной части

--- CommandServerMVC/Program.cs
+++ CommandServerMVC/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//builder.WebHost.UseUrls("http://*:5000");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
