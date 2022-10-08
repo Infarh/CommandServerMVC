19e4f561cdf61f2bb9ef42f877eb69a40f7d09de
Infarh <shmachilin@yandex.ru>
2022-10-08 14:59:37 +0300

    Добавьте файлы проекта.

---
+++ CommandServerMVC/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+using CommandServerMVC.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandServerMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger) => _logger = logger;
+
+    public IActionResult Index() => View();
+
+    [HttpPost]
+    public IActionResult Execute(CommandInfo Command)
+    {
+
+
+        return RedirectToAction("Index");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+}
