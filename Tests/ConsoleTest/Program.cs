10c1e252b17fa28c1ed35e08493dd1007ae468a8
Infarh <shmachilin@yandex.ru>
2022-10-10 00:48:32 +0300

    Клиент для отправки файла на сервер

---
+++ Tests/ConsoleTest/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using CommandClient;
+
+using var http = new HttpClient()
+{
+    BaseAddress = new("http://localhost:1423"),
+};
+
+var client = new FilesClient(http);
+
+Console.WriteLine("Ожидание сервера");
+Console.ReadLine();
+
+await client.UploadAsync(@"c:\123\Diagram 2022-07-06 20-48-12.uxf");
+
+Console.WriteLine("End!");
